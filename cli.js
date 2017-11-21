b8666ff3c6f44d14ce9269e34169afd0df285a3d
Gordon Hall <gordon@gordonwritescode.com>
2017-11-21 16:04:09 -0500

    create a hidden services v3 wrapper

---
+++ cli.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const program = require('commander');
+const hsv3 = require('./index');
+
+
+program
+  .version(require('./package').version)
+  .description('wrapper for ad-hoc establishing version 3 tor hidden services')
+  .option('-d, --datadir <path>', 'hidden service data directory')
+  .option('-p, --virtport <port>', 'virtual port for hidden service')
+  .option('-m, --mapping <ip:port>', 'target to expose as hidden service')
+  .option('-l, --log', 'passthrough tor process logging')
+  .parse(process.argv);
+
+const receivedValidOptions = program.datadir && program.virtport &&
+  program.mapping;
+
+if (!receivedValidOptions) {
+  program.help();
+  process.exit(1);
+}
+
+const tor = hsv3([
+  {
+    dataDirectory: program.datadir,
+    virtualPort: program.virtport,
+    localMapping: program.mapping
+  }
+]);
+
+tor.on('error', (err) => {
+  console.error('  ');
+  console.error('  ' + err.message);
+  process.exit(1);
+});
+
+tor.on('ready', () => {
+  const hostname = fs.readFileSync(path.join(program.datadir, 'hostname'))
+    .toString();
+
+  console.info('  ');
+  console.info('  V3 Hidden Service Established: ' + hostname);
+
+  if (program.log) {
+    console.info('  ');
+    tor.process.stdout.pipe(process.stdout);
+  }
+});
---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * @module hsv3
+ */
+
+'use strict';
+
+/**
+ * Starts a Tor process with the given v3 hidden services configured
+ * @param {array} services
+ * @param {object} services[].dataDirectory - Data directory for hidden service
+ * @param {number} services[].virtualPort - Hidden service virtual port
+ * @param {string} services[].localMapping - Local service to map
+ */
+module.exports = function(services) {
+  return require('granax')({ authOnConnect: true }, services.map(block => {
+    return {
+      HiddenServiceDir: block.dataDirectory,
+      HiddenServiceVersion: 3,
+      HiddenServicePort: `${block.virtualPort} ${block.localMapping}`
+    };
+  }));
+};
