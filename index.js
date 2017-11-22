644dd54a6908156fb9a3590817fa65d815d97bf0
Gordon Hall <gordon@gordonwritescode.com>
2017-11-22 12:52:02 -0500

    allow additional torrc entries

--- index.js
+++ index.js
@@ -10,13 +10,14 @@
  * @param {object} services[].dataDirectory - Data directory for hidden service
  * @param {number} services[].virtualPort - Hidden service virtual port
  * @param {string} services[].localMapping - Local service to map
+ * @param {object} torrc - Additional torrc entries
  */
-module.exports = function(services) {
+module.exports = function(services, torrc = {}) {
   return require('granax')({ authOnConnect: true }, services.map(block => {
     return {
       HiddenServiceDir: block.dataDirectory,
       HiddenServiceVersion: 3,
       HiddenServicePort: `${block.virtualPort} ${block.localMapping}`
     };
-  }));
+  }).concat([torrc]));
 };
